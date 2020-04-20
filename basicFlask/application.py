d76d210f54db9d5fec1dffd4400740b7f7219646
Shivaram Shreevardhan <shreevardhan.shivaram@nagra.com>
2020-04-20 12:24:46 +0530

    changed folder structure

--- basicFlask/application.py
+++ basicFlask/application.py
@@ -6,6 +6,14 @@ app = Flask(__name__)  # new Flask web application; __name__ represents the name
 def index():
     return "Hello there!"
 
+@app.route("/shree")
+def shree():
+    return "Hello, Shree!"
+
+@app.route("/<string:name>")
+def hello(name):
+    return(f"Hello, {name}!")
+
 
 if __name__ == "__main__":
     app.run()
