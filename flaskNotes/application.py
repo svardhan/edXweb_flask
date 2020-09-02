39ce4e3a6da9f099e9b726d2b3f373a1103372f3
Shivaram Shreevardhan <shreevardhan.shivaram@nagra.com>
2020-09-02 15:56:45 +0530

    Minor changes

--- flaskNotes/application.py
+++ flaskNotes/application.py
@@ -8,7 +8,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 #notes = [] # single notes variable shared by everyone.and
-           # note desirable
+           # not desirable
 
 @app.route("/", methods=["GET", "POST"])
 def index():
